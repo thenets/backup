e0de29d1ce48c112bf1e0eccefe35687d497e0e0
Luiz Costa <luiz@thenets.org>
2019-10-15 04:22:07 -0300

    First rsync finally working

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -38,6 +38,7 @@ type SSH struct {
 // and the local dir
 func (ssh *SSH) Sync() {
 	var debug = true
+	var runOverBash = true
 
 	// Create cache dir
 	var backupCacheDir = utils.GetCacheDir() + ssh.Metadata.ID + "/"
@@ -52,19 +53,29 @@ func (ssh *SSH) Sync() {
 	var commandName = "/usr/bin/rsync"
 	var args []string
 	args = []string{
-		"-e",
-		fmt.Sprintf("\"ssh -p %d %s\"", ssh.Spec.Server.Port, ssh.Spec.CustomSSHArgs),
 		"-av",
 		"--timeout=300",
 		"--recursive",
 		"--delete",
+		"-e",
+		fmt.Sprintf("'ssh -p %d %s'", ssh.Spec.Server.Port, ssh.Spec.CustomSSHArgs),
 		fmt.Sprintf("%s@%s:%s", ssh.Spec.Server.User, ssh.Spec.Server.Host, ssh.Spec.RemoteDir),
 		backupCacheDir,
 	}
-	args = append(args, strings.Split(ssh.Spec.CustomSSHArgs, " ")...)
+	args = append(args, strings.Split(ssh.Spec.CustomRsyncArgs, " ")...)
 	if debug {
 		log.Println("CMD", commandName)
 		log.Println("ARG", args)
+	}
+
+	// Run over bash
+	if runOverBash {
+		var newCommandName = "/bin/sh"
+		var newArgs = []string{"-c"}
+		newArgs = append(newArgs, strings.Join(append([]string{commandName}, args...), " "))
+
+		commandName = newCommandName
+		args = newArgs
 	}
 
 	// Set log files
